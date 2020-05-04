457ad6e036132a6a47fa79ee3ecfe38c1e219118
Deshi Xiao <xiaods@gmail.com>
2020-05-04 20:56:08 +0800

    update client status logic

--- BinanceClient.py
+++ BinanceClient.py
@@ -4,6 +4,7 @@ from binance.client import Client
 from binance.enums import *
 from binance.websockets import BinanceSocketManager
 import time
+from datetime import datetime
 
 from BinanceKeys import BinanceKey1
 
@@ -11,30 +12,80 @@ api_key = BinanceKey1['api_key']
 api_secret = BinanceKey1['api_secret']
 client = Client(api_key, api_secret)
 
+# 配置参数
 symbol = 'EOSUSDT'
 coin_symbol = 'EOS'
 usdt_symbol = 'USDT'
+bnb_symbol = 'BNB'
 max_margins = 15
+# 投入本金，手工统计
+base_balance = 1134
+fiat_symbol = 'CNY'
+fiat_price = 7.1
 
 def run():
-    print('=========start running=================')
+    print('='*30)
+    get_account_status()
 
-    print("current orders")
-    get_all_margin_orders()
+def get_account_status():
+    btcusdt_avg_price = client.get_avg_price(symbol='BTCUSDT')
+    bnbusdt_avg_price = client.get_avg_price(symbol='BNBUSDT')
+    current_btc_price = float(btcusdt_avg_price.get('price'))
+    current_bnb_price = float(bnbusdt_avg_price.get('price'))
+    message = "QCat Auto Trading bot\n"
+    message += "-"*30 
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    message += "\n统计时间：%s" % dt_string
+    message += "\n当前BTC价格: {:.2f} USDT\n".format(current_btc_price)
+    message += "当前BNB价格: {:.2f} USDT\n\n".format(current_bnb_price)
 
-    print('current free assets')
-    get_free_assets()
-
-def get_free_assets():
+    # 计算账户信息 start
     account = client.get_margin_account()
     userAssets = account.get('userAssets')
     free_coin = float(0)
     free_cash = float(0)
+    loan_coin = float(0)
+    net_coin = float(0)
+    bnb_free_coin = float(0)
     for asset in userAssets:
         if asset.get('asset') == coin_symbol:
             free_coin = asset.get('free')
+            loan_coin = asset.get('borrowed')
+            net_coin = asset.get('netAsset')
+        if asset.get('asset') == bnb_symbol:
+            bnb_free_coin = asset.get('free')
         if asset.get('asset') == usdt_symbol:
             free_cash = asset.get('free')
+    # 计算账号信息 end
+
+    # 总投入成本
+    base_bnb_balance = float(bnb_free_coin)
+    total_base_balance = float(base_balance) + float(base_bnb_balance * current_bnb_price)
+    # 利润
+    profit_balance = (float(account.get("totalNetAssetOfBtc")) *  current_btc_price) - total_base_balance
+    # 利润率
+    profit_percent = float(profit_balance / total_base_balance)
+    message += "*** 投入本金: {} USDT, {} BNB (约为{}: {:.2f}) ***\n".format(base_balance, base_bnb_balance, fiat_symbol, total_base_balance)
+    message += "*** 日累计盈亏: {0:.2f} USDT(约为{1}: {2:.2f} )，日利润率{3:.3%} ***\n\n".format(profit_balance, fiat_symbol, float(profit_balance * fiat_price), profit_percent)
+    message += "-"*30
+    message += "\n杠杆账号资产详情：\n"
+    message += "\n当前杠杆账号风险率(默认3倍杠杆，小于2表示危险，需要人工处理): %s\n"  % account.get("marginLevel")
+    message += "当前杠杆账号总资产估值: {0} BTC (约为 {1:.2f} USDT )\n".format( account.get("totalAssetOfBtc"), float(account.get("totalAssetOfBtc")) *  current_btc_price )
+    message += "当前杠杆账号账户权益: {0} BTC (约为 {1:.2f} USDT )\n".format( account.get("totalNetAssetOfBtc"), float(account.get("totalNetAssetOfBtc")) * current_btc_price  )
+
+    message += "当前帐户可用现金{0}是: {1}\n".format(usdt_symbol, float(free_cash) ) 
+    message += "当前账户可用代币{0}是: {1}\n".format(coin_symbol, float(free_coin) )
+    print(message)
+    asset_message = "当前借入{}资产是{}\n".format(coin_symbol, loan_coin)
+    asset_message += "当前{}的净资产是{}\n".format(coin_symbol, net_coin)
+    print(asset_message)
+
+
+def get_free_assets():
+    account = client.get_margin_account()
+    userAssets = account.get('userAssets')
+
     print("free cash: %.4f " % float(free_cash) )
     print("free coin: %.4f " % float(free_coin) )
 
