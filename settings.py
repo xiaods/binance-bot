badef78880545df7126cc35e657c864a314aa947
Deshi Xiao <xiaods@gmail.com>
2020-05-04 22:28:02 +0800

    add cron jobs to send profit email

---
+++ settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+ZohoMail = {'username': 'your username',
+    'password':'your password',
+    'smtpserver': 'your smtp server',
+    'serverport': 465,
+    'recipient': ['xxx@gmail.com'],
+    }
