44c3803116855c88a711df41627c5e6c05862045
Deshi Xiao <xiaods@gmail.com>
2020-05-05 08:45:22 +0800

    update orders scripts

--- copyfile.sh
+++ copyfile.sh
@@ -6,7 +6,9 @@ echo "=========================================="
 
 echo copy release files to release folder
 
-cp BinanceBot.py BinanceTop.py BinanceKeys.py ProfitReport.py rebuy_coin.py resell_coin.py settings.py ./release/
+cp  BinanceBot.py BinanceTop.py BinanceKeys.py \
+    ProfitReport.py rebuy_coin.py resell_coin.py \
+    settings.py BinanceOrders.py ./release/
 
 cp ./scripts/*.sh ./release/
 
