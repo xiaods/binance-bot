bac849a7a89d961d6073ddea3e6b2be106b942f9
Deshi Xiao <xiaods@gmail.com>
2020-05-05 19:33:22 +0800

    add rebuy_bnb.py to copyfile.sh

--- copyfile.sh
+++ copyfile.sh
@@ -8,6 +8,7 @@ echo copy release files to release folder
 
 cp  BinanceBot.py BinanceTop.py BinanceKeys.py \
     ProfitReport.py rebuy_coin.py resell_coin.py \
+    rebuy_bnb.py \
     settings.py BinanceOrders.py ./release/
 
 cp ./scripts/*.sh ./release/
