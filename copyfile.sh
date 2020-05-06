dde3f3091b1f2b3fce39f1d05d02c1d7b76116d1
Deshi Xiao <xiaods@gmail.com>
2020-05-06 22:40:47 +0800

    tuning project structure

--- copyfile.sh
+++ copyfile.sh
@@ -8,11 +8,10 @@ rm -rf release && mkdir release
 
 echo "copy core bot file to release..."
 cp  binance_bot.py binance_top.py binance_orders.py binance_report.py \
-    settings.py ./release/
+    settings_tpl.py ./release/
 
 echo "some useful scripts for trading..."
-cp  rebuy_bnb.py rebuy_coin.py repay_margin_loan.py \
-    resell_coin.py ./release/
+cp  -avr ./utility ./release/
 
 echo "some bash script to get thing start..."
 cp ./scripts/*.sh ./release/
---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Welcome to binance bot manager!"
+
+echo "First, copy settings_tpl.py to settings.py..."
+
+cp settings_tpl.py settings.py
+
+echo "Second, edit settings.py to tuning parameters..."
+
+echo "./start.sh to bootstrap the bot service..."
+
+echo "Done!"
