8e5ca19a0fe084deb1bd7652cfb8d07e5ff7ff92
Deshi Xiao <xiaods@gmail.com>
2020-05-20 23:39:27 +0800

    remove rsi bot

--- scripts/rsi_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "start strading bot"
-echo "====================="
-
-python3 bnb_stochrsi_bot.py > bot.log 2>&1 & 
-
-echo "Done!"
--- scripts/rsi_stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "stop trading bot"
-echo "===================="
-
-# 停止机器人
-kill -9 $( ps aux|grep bnb_stochrsi_bot.py|grep -v grep|awk '{print $2}' )
-
-echo "Done!"
