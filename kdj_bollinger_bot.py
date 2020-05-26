4a5cd6d186604e285534a5fedf08ae2ac11ba42e
Deshi Xiao <xiaods@gmail.com>
2020-05-26 21:57:35 +0800

    add new rule for kdj

--- kdj_bollinger_bot.py
+++ kdj_bollinger_bot.py
@@ -173,15 +173,17 @@ def kdj_signal_trading(symbol):
     
     # 交易策略，吃多单
     if check_range(float(np_k[-1]) - float(np_d[-1])) and \
+        float(np_k[-1]) < float(30) and float(np_j[-1]) < float(30) and \
         float(np_low_data[-1]) <= float(cur_dn) and len(long_order) < max_margins/2 and \
-         (order_dt_ended - order_dt_started).total_seconds() > 60*5:
+        (order_dt_ended - order_dt_started).total_seconds() > 60*5:
 
         indicator = "LONG"  # 做多
         new_margin_order(symbol,qty,indicator)  #  下单
         order_dt_started = datetime.utcnow()  # 5分钟只能下一单
     elif check_range(float(np_k[-1]) - float(np_d[-1])) and \
+         float(np_k[-1]) > float(70) and float(np_j[-1]) > float(70) and \
         float(np_high_data[-1]) >= float(cur_up) and len(short_order) < max_margins/2 and \
-          (order_dt_ended - order_dt_started).total_seconds() > 60*5:
+        (order_dt_ended - order_dt_started).total_seconds() > 60*5:
 
         indicator = "SHORT" # 做空
         new_margin_order(symbol,qty,indicator)  #  下单
